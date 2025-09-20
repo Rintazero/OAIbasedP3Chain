1b6b96d2f880ef5bcb39424426fb89dd78c3a777
rintazero <circode@126.com>
2025-09-20 13:14:46 +0800

    fix: 完善 rfsim_dev docker 编译链

--- executables/nr-uesoftmodem.c
+++ executables/nr-uesoftmodem.c
@@ -394,7 +394,7 @@ int main(int argc, char **argv)
   logInit();
   // modified at 20250919-1624 #my_mark
   printf("rfsim_dev: built %s %s\n", __DATE__, __TIME__);
-  LOG_I(HW, "rfsim_dev build %s %s\n", __DATE__, __TIME__);
+  LOG_I(UTIL, "rfsim_dev build %s %s\n", __DATE__, __TIME__);
   // get options and fill parameters from configuration file
 
   get_options(uniqCfg); // Command-line options specific for NRUE
